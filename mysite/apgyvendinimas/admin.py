dd7031bfb4bbdfe2eeaa483d2ccf3f353aa55868
TomasNorvilis <tomasnorvilis@yahoo.com>
2023-02-15 18:12:49 +0200

    ideta paiska pagal miesta ir tipa apgyvendinimo

--- mysite/apgyvendinimas/admin.py
+++ mysite/apgyvendinimas/admin.py
@@ -6,5 +6,8 @@ from .models import Objektas
 # Register your models here.
 class ObjektasAdmin(admin.ModelAdmin):
     list_display = ('caption', 'type', 'city', 'area', 'rooms', 'max_guest')
+    list_filter = ('type', 'city')
+    search_fields = ['type','city']
+
 
 admin.site.register(Objektas, ObjektasAdmin)
