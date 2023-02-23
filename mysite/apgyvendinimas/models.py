c70f53bdeb5d7ecb2bc9739f8712229f49882c8b
TomasNorvilis <tomasnorvilis@yahoo.com>
2023-02-23 14:16:06 +0200

    pridetas profilio nuotrauku sumazinimas pagal parametrus pasirinktus

--- mysite/apgyvendinimas/models.py
+++ mysite/apgyvendinimas/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from tinymce.models import HTMLField
 from datetime import date
+from PIL import Image
 
 
 # Create your models here.
@@ -50,6 +51,14 @@ class Profilis(models.Model):
 
     def __str__(self):
         return f"{self.user.username} profilis"
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        img = Image.open(self.foto.path)
+        if img.height > 200 or img.width > 200:
+            output_size = (200, 200)
+            img.thumbnail(output_size)
+            img.save(self.foto.path)
 
 
     class Meta:
